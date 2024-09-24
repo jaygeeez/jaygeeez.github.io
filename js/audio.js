5c34ec3404dd461b9d350ae2b5fad7fb8d4ecdb0
jaygeeez <66766785+jaygeeez@users.noreply.github.com>
2024-09-24 19:54:47 -0400

    added items

---
+++ js/audio.js
@@ -0,0 +1,5 @@
+function playAudio() {
+  var piano1 = new Audio();
+  piano1.src = "audio/piano1.wav";
+  piano1.play();
+}
---
+++ js/darkmode.js
@@ -0,0 +1,11 @@
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
+var apple = 1 + 1;
+console.log(apple);
+
+// curl -X POST "https://accounts.spotify.com/api/token" \
+//      -H "Content-Type: application/x-www-form-urlencoded" \
+//      -d "grant_type=client_credentials&client_id=b0967dcab86e44698289a22765cf49dc&client_secret=14aa4e5fce0443a4a4bca54c5ba51ba2"
