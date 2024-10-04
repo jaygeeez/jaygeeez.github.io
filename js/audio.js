8b778c0ae3bbd8d92a83aa334dd8a8ca1cabcd84
jaygeeez <66766785+jaygeeez@users.noreply.github.com>
2024-10-04 11:36:32 -0400

    removed unused sound, sound on dark toggle

--- js/audio.js
+++ js/audio.js
@@ -1,5 +1,6 @@
 function playAudio() {
   var piano1 = new Audio();
+  piano1.pause(); // Trying to get page to stop all other sounds before playing it again
   piano1.src = "audio/piano1.wav";
   piano1.play();
 }
