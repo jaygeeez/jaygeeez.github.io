50900bc225fcfb656b847237cc58a557352cd992
jaygeeez <66766785+jaygeeez@users.noreply.github.com>
2024-10-08 17:45:38 -0400

    fixed audio

--- js/audio.js
+++ js/audio.js
@@ -1,6 +1,9 @@
 function playAudio() {
   var piano1 = new Audio();
-  piano1.pause(); // Trying to get page to stop all other sounds before playing it again
   piano1.src = "audio/piano1.wav";
+  piano1.loop = true;
   piano1.play();
+  setTimeout(() => {
+    piano1.pause();
+  }, 2500);
 }
