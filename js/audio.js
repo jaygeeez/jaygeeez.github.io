f057c00fad8f40dd8dfdf9c3ae16e44176a3649b
jaygeeez <66766785+jaygeeez@users.noreply.github.com>
2024-10-12 23:23:52 -0400

    music switcher

--- js/audio.js
+++ js/audio.js
@@ -1,10 +1,25 @@
-// try randomizing or having audio different based on light or dark
+var dark_mode = true;
+
 function playAudio() {
   var piano1 = new Audio();
   piano1.src = "audio/piano1.wav";
-  piano1.loop = true;
-  piano1.play();
-  setTimeout(() => {
-    piano1.pause();
-  }, 6000);
+
+  var piano3 = new Audio();
+  piano3.src = "audio/piano3.wav";
+
+  if (dark_mode) {
+    piano3.loop = true;
+    piano3.play();
+    setTimeout(() => {
+      piano3.pause();
+    }, 6000);
+    dark_mode = false;
+  } else {
+    piano1.loop = true;
+    piano1.play();
+    setTimeout(() => {
+      piano1.pause();
+    }, 6000);
+    dark_mode = true;
+  }
 }
